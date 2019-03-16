10e1188aef4bd2aa21822b036f293f5f1b73215d
elihaimov1992 <37331405+elihaimov1992@users.noreply.github.com>
2019-03-16 19:34:05 +0200

    Update Player.h

--- Player.h
+++ Player.h
@@ -3,9 +3,10 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 #include "Card.h"
-#include <string>
 #include <vector>
-
+#include<string>
+#include<iostream>
+using namespace std;
 class Player {
 private:
 	//private members
@@ -19,22 +20,19 @@ public:
 	//public members and functions
 	//C'tors
 	Player(){};
-	Player(string name,int num_of_cards) :name(name), num_of_cards(num_of_cards), cur_sign(NAS){};
+	Player(string name,int num_of_cards) :name(name), num_of_cards(num_of_cards), curr_sign(sign::NAS){};
 	Player(const Player& p);
-	Player operator= (Const Player& p);
+	Player operator= (const Player& p);
 	//Functions
-	bool play(Card&); 
+	bool play(Card& c); 
 	void add_cards(const int& num_of_cards); //add cards to the deck
-	bool is_winner() const{return Card.empty();} //return true if the player has no cards and becomes a winner
+	bool is_winner() const{return cards.empty();} //return true if the player has no cards and becomes a winner
 	//Getters & Setters
 	sign getCurr_sign() const{return curr_sign;}
-	void setCurr_sign(const sign& new_curr_sign) :curr_sign(new_curr_sign){};
+	void setCurr_sign(const sign& new_curr_sign) {curr_sign = new_curr_sign;}
 	string getName() const{return name;}
-	void reset_curr_sign() {curr_sign=NAS;}
+	void reset_curr_sign() {curr_sign = sign::NAS;}
 	//D'Ctor
 	//~Player(); 
 };
 #endif
-
-
-
